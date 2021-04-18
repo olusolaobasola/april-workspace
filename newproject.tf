ff8abf14b4cfa22a3106a72bb19ecbae51288a2f
olusola obasola <olusola.obasola@gmail.com>
2021-04-18 09:25:26 +0100

    adding newproject file to repo

---
+++ newproject.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+
+resource "aws_instance" "new_project" {
+  ami                    = "ami-0fbec3e0504ee1970"
+  instance_type          = "t2.micro"
+  key_name               = "DAY 2 HOMEWORK"
+  vpc_security_group_ids = [aws_security_group.newproject_sg.id]
+
+  tags = {
+    Name        = "newproject"
+    provisioner = "Terraform"
+  }
+}
+
+resource "aws_security_group" "newproject_sg" {
+  name = "newproject2_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
