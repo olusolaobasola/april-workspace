a46e2de7ddfb83ce97fec51a9c1fbb62e94ac440
olusola obasola <olusola.obasola@gmail.com>
2021-04-20 23:27:04 +0100

    adding jenkins instance

--- newproject.tf
+++ newproject.tf
@@ -17,6 +17,19 @@ resource "aws_instance" "new_project" {
   }
 }
 
+resource "aws_instance" "jenkins_newproject" {
+  ami                    = "ami-0fbec3e0504ee1970"
+  instance_type          = "t2.micro"
+  key_name               = "DAY 2 HOMEWORK"
+  vpc_security_group_ids = [aws_security_group.newproject_sg.id]
+
+  tags = {
+    Name        = "jenkins-newproject"
+    provisioner = "Terraform"
+    test        = "yes"
+  }
+}
+
 resource "aws_security_group" "newproject_sg" {
   name = "newproject2_sg"
 
@@ -31,6 +44,13 @@ resource "aws_security_group" "newproject_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
